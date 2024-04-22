6783ee54be032064732abab4a33a1f9bdf98cecb
hi-euu <hieu05992@gmail.com>
2024-04-22 15:24:53 +0700

    fix: handle exception

--- src/main/java/banhmilos/webscrapeai/ai/NotegptSummaryAI.java
+++ src/main/java/banhmilos/webscrapeai/ai/NotegptSummaryAI.java
@@ -1,20 +1,29 @@
 package banhmilos.webscrapeai.ai;
 
-import com.google.gson.JsonElement;
+import banhmilos.webscrapeai.util.URLValidator;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class NotegptSummaryAI implements SummaryAI {
 
     @Override
-    public String summary(String url) throws IOException {
-        String api = "https://notegpt.io/api/v1/ai-tab/gpts-summary?url=" + URLEncoder.encode(url, "UTF-8");
+    public String summaryUrl(String url) throws IOException {
+
+        if (!URLValidator.isValidUrl(url)) throw new RuntimeException("Not valid url");
+
+        String api = "https://notegpt.io/api/v1/ai-tab/gpts-summary?url=" + URLEncoder.encode(url, StandardCharsets.UTF_8);
         String response = Jsoup.connect(api).ignoreContentType(true).get().body().text();
-        JsonElement json = JsonParser.parseString(response);
-        String result = json.getAsJsonObject().get("data").getAsJsonObject().get("text").getAsString();
-        return result;
+
+        JsonObject json = JsonParser.parseString(response).getAsJsonObject();
+        if (!json.has("data")) throw new RuntimeException("No data");
+        JsonObject data = json.get("data").getAsJsonObject();
+        if (!data.has("text")) throw new RuntimeException("No text");
+
+        return data.get("text").getAsString();
     }
 }
